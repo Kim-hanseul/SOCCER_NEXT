b496e4533110da39c84356abb1d77e6df01906b2
Kim-hanseul <hansul80@gmail.com>
2022-04-13 17:48:15 +0900

    2022.04.13

--- pages/common/Nav.js
+++ pages/common/Nav.js
@@ -6,16 +6,19 @@ export default function Nav(){
   return (
     <nav className={styles.nav}>
       <ul>
-        <li className={styles.li}> <Link href='/'>Home</Link> </li>
+        <li className={styles.li}> <Link href='/'>Main</Link> </li>
         <li className={styles.li}> <Link href='/admin/dashboard'>DashBoard</Link> </li>
         <li className={styles.li}> <Link href='/basic/calc'>Calculator</Link> </li>
         <li className={styles.li}> <Link href='/basic/counter'>Counter</Link> </li>
         <li className={styles.li}> <Link href='/basic/bmi'>BMI</Link> </li>
         <li className={styles.li}> <Link href='/board/board-list'>Board List</Link> </li>
-        <li className={styles.li}> <Link href='/board/board-form'>Board form</Link> </li>
-        <li className={styles.li}> <Link href='/game/game-list'>GameList</Link> </li>
-        <li className={styles.li}> <Link href='/game/team-list'>TeamList</Link> </li>
-        <li className={styles.li}> <Link href='/todo/todo-list'>Schedule</Link> </li>
+        <li className={styles.li}> <Link href='/board/board'>board regist</Link> </li>
+        <li className={styles.li}> <Link href='/game/game'>game regist</Link> </li>
+        <li className={styles.li}> <Link href='/game/game-list'>Game List</Link> </li>
+        <li className={styles.li}> <Link href='/game/team'>Team regist</Link> </li>
+        <li className={styles.li}> <Link href='/game/team-list'>Team List</Link> </li>
+        <li className={styles.li}> <Link href='/todo/todo'>Schedule regist</Link> </li>
+        <li className={styles.li}> <Link href='/todo/todo-list'>Schedule list</Link> </li>
         <li className={styles.li}> <Link href='/user/login'>Login</Link> </li>
         <li className={styles.li}> <Link href='/user/join'>join</Link> </li>
         <li className={styles.li}> <Link href='/user/user-list'>UserList</Link> </li>
--- redux/reducers/board.reducer.js
+++ redux/reducers/board.reducer.js
@@ -1,11 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {}
-
+const initialState = []
 export const boardSlice = createSlice({
-    name: 'boards',
-    initialState,
-    reducers: {}
+     name: 'boards',
+     initialState,
+     reducers : {
+          addBoard: (state,action)=>{
+               alert('리듀서 내부에서 할일은 ?'+ action.payload.board)
+               const board ={id: new Date(), task :action.payload.board,completed:false}
+               state.push(board)
+          }
+     
+     }
 })
-
+export const {addBoard} = boardSlice.actions
 export default boardSlice.reducer
